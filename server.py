c875b0087112ad2f8b2c0cc0481b129451b5b9d3
deysnight <36400307+deysnight@users.noreply.github.com>
2020-05-23 22:39:38 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 
 from flask import Flask, url_for, render_template
 import random
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+DIG_PIN = 11
+ANA_PIN = 13
+GPIO.setup(DIG_PIN, GPIO.IN)
+GPIO.setup(ANA_PIN, GPIO.IN)
+
 
 app = Flask(__name__)
 
@@ -11,6 +21,12 @@ def home():
 @app.route('/sensors')
 def config():
     
-    res = random.uniform(1, 10)
+    res_ana = GPIO.input(ANA_PIN);
+    res_dig = GPIO.input(DIG_PIN);
     
-    return str('{"dig": "day", "ana": ' + str(res) + '}')
+    if (res_dig):
+        state = "day"
+    else:
+        state = "night"
+    
+    return str('{"dig": "' + state + '", "ana": ' + str(res_ana) + '}')
